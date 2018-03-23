797fef31568f1dfbc396144b6810c4fdf6bcfce9
Nick-Damico <nickalan82@icloud.com>
2018-03-23 15:02:52 -0400

    spec test for removing contact from a address book finished complete

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,13 +1,24 @@
 // First add new suite.
 
 describe('Address Book', function() {
-  it('should be able to add a contact', function(){
-    // using an Object Oriented Approach
-    var addressBook = new AddressBook(),
-        thisContact = new Contact();
+  var addressBook,
+      thisContact;
 
+  beforeEach(function(){
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
+  it('should be able to add a contact', function(){
     // now to add a contact to the instance of address book.
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+
+  it('should be able to delete a contact', function(){
+    addressBook.addContact(thisContact);
+    addressBook.removeContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,16 @@
 function AddressBook() {
   this.contacts = [];
 
-  this.addContact = function(contact) {
-    this.contacts = [...this.contacts, contact];
-  }
+};
 
-  this.getContact = function(index) {
-    return this.contacts[ index ];
-  }
-}
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts = [...this.contacts, contact];
+};
+
+AddressBook.prototype.getContact = function(index){
+  return this.contacts[ index ];
+};
+
+AddressBook.prototype.removeContact = function(index){
+  this.contacts.splice(index, 1);
+};
